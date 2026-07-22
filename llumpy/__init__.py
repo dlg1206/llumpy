68c741b60eea19057bccdb50c6a91e376478f3ef
Derek Garcia <dcia811@gmail.com>
2026-07-21 15:36:49 -1000

    feat!: Remove all bulk export and enforce namespaces
    before: from llumpy import OpenAIClient
    after: from llumpy.client import OpenAIClient

--- llumpy/__init__.py
+++ llumpy/__init__.py
@@ -5,39 +5,3 @@ Description:
 
 @author Derek Garcia
 """
-from .client import (AnthropicClient as AnthropicClient,
-                     AsyncAnthropicClient as AsyncAnthropicClient,
-                     OllamaClient as OllamaClient,
-                     AsyncOllamaClient as AsyncOllamaClient,
-                     OpenAIClient as OpenAIClient,
-                     AsyncOpenAIClient as AsyncOpenAIClient)
-
-from .core import (Role as Role,
-                   Message as Message,
-                   Conversation as Conversation,
-                   ConversationBuilder as ConversationBuilder,
-                   ModelClient as ModelClient,
-                   AsyncModelClient as AsyncModelClient,
-                   InvalidAPIKeyError as InvalidAPIKeyError,
-                   ModelNotFoundError as ModelNotFoundError)
-
-from .retry import (ExceededRetriesError as ExceededRetriesError,
-                    RetryHandler as RetryHandler,
-                    AsyncRetryHandler as AsyncRetryHandler,
-                    JSONRetryHandler as JSONRetryHandler,
-                    AsyncJSONRetryHandler as AsyncJSONRetryHandler)
-
-__all__ = [
-    # clients
-    "OpenAIClient", "AsyncOpenAIClient",
-    "AnthropicClient", "AsyncAnthropicClient",
-    "OllamaClient", "AsyncOllamaClient",
-    # core
-    "ConversationBuilder", "Role", "Message", "Conversation",
-    "ModelClient", "AsyncModelClient",
-    "InvalidAPIKeyError", "ModelNotFoundError",
-    # retry
-    "ExceededRetriesError",
-    "RetryHandler", "AsyncRetryHandler",
-    "JSONRetryHandler", "AsyncJSONRetryHandler"
-]
