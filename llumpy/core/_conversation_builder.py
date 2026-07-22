7f6e57644a80732f85dc207f07d5ecfd8b69bc4a
Derek Garcia <dcia811@gmail.com>
2026-07-21 16:37:32 -1000

    fix: Prevent building conversation with just a system prompt

--- llumpy/core/_conversation_builder.py
+++ llumpy/core/_conversation_builder.py
@@ -5,36 +5,15 @@ Description: Builder to ensure conversations to LLMs are in the correct order
 
 @author Derek Garcia
 """
-from abc import ABC
 from typing import List, Literal
 
 from ._args import _validate_args
 from ._models import Role, Message, Conversation
 
 
-class _BuildMixIn(ABC):
-    def __init__(self, messages: List[Message]):
-        """
-        Create new mixin
-
-        :param messages: Messages in the current conversation
-        """
-        self._messages = messages
-
-    def _build_with(self,
-                    role: Literal[Role.USER, Role.ASSISTANT],
-                    content: str | None = None,
-                    file: str | None = None) -> Conversation:
-        """
-        Build a conversation without appending the last message to the current conversation
-
-        :param role: Either temporary user or assistant role to the conversation
-        :param content: Content of system message (Default: None)
-        :param file: File to read content from (Default: None)
-        :return: Immutable conversation
-        """
-        content = _validate_args(content, file)
-        return Conversation(self._messages + [Message(role, content)])
+class _BuildMixIn:
+    """Mixin that allows a step to build a conversation"""
+    _messages: List[Message]  # assert self._messages will be available
 
     def build(self) -> Conversation:
         """
@@ -68,33 +47,8 @@ class _MessagesHolder:
         return self._messages
 
 
-class ConversationBuilder(_MessagesHolder):
-    """Conversation builder to ensure messages are in a valid order"""
-
-    def system(self, content: str | None = None, *, file: str | None = None) -> "_UserStepOrBuildStep":
-        """
-        Add a system message
-
-        :param content: Content of system message (Default: None)
-        :param file: File to read content from (Default: None)
-        :raises ValueError: If nether or both content or file provided
-        :return: User step
-        """
-        return _UserStepOrBuildStep(self._add(Role.SYSTEM, content, file))
-
-    def user(self, content: str | None = None, *, file: str | None = None) -> "_AssistantOrBuildStep":
-        """
-        Add a user message
-
-        :param content: Content of system message (Default: None)
-        :param file: File to read content from (Default: None)
-        :return: Assistant or build step
-        """
-        return _AssistantOrBuildStep(self._add(Role.USER, content, file))
-
-
-class _UserStepOrBuildStep(_MessagesHolder, _BuildMixIn):
-    """User turn in the conversation"""
+class _UserStep(_MessagesHolder):
+    """First user turn - prevent building with just a system prompt"""
 
     def user(self, content: str | None = None, *, file: str | None = None) -> "_AssistantOrBuildStep":
         """
@@ -114,13 +68,18 @@ class _UserStepOrBuildStep(_MessagesHolder, _BuildMixIn):
         :param file: File to read content from (Default: None)
         :return: List of messages
         """
-        return self._build_with(Role.USER, content, file)
+        return _build_with(self._messages, Role.USER, content, file)
+
+
+class _UserOrBuildStep(_UserStep, _BuildMixIn):
+    """User turn in the conversation - allow build"""
+    pass
 
 
 class _AssistantOrBuildStep(_MessagesHolder, _BuildMixIn):
     """Assistant or build step in the conversation"""
 
-    def assistant(self, content: str | None = None, *, file: str | None = None) -> "_UserStepOrBuildStep":
+    def assistant(self, content: str | None = None, *, file: str | None = None) -> "_UserOrBuildStep":
         """
         Add an assistant message
 
@@ -128,7 +87,7 @@ class _AssistantOrBuildStep(_MessagesHolder, _BuildMixIn):
         :param file: File to read content from (Default: None)
         :return: User step
         """
-        return _UserStepOrBuildStep(self._add(Role.ASSISTANT, content, file))
+        return _UserOrBuildStep(self._add(Role.ASSISTANT, content, file))
 
     def build_with_assistant(self, content: str | None = None, *, file: str | None = None) -> Conversation:
         """
@@ -138,4 +97,46 @@ class _AssistantOrBuildStep(_MessagesHolder, _BuildMixIn):
         :param file: File to read content from (Default: None)
         :return: List of messages
         """
-        return self._build_with(Role.ASSISTANT, content, file)
+        return _build_with(self._messages, Role.ASSISTANT, content, file)
+
+
+class ConversationBuilder(_MessagesHolder):
+    """Conversation builder to ensure messages are in a valid order"""
+
+    def system(self, content: str | None = None, *, file: str | None = None) -> _UserStep:
+        """
+        Init conversation with a system message
+
+        :param content: Content of system message (Default: None)
+        :param file: File to read content from (Default: None)
+        :raises ValueError: If nether or both content or file provided
+        :return: User step
+        """
+        return _UserStep(self._add(Role.SYSTEM, content, file))
+
+    def user(self, content: str | None = None, *, file: str | None = None) -> _AssistantOrBuildStep:
+        """
+        Init conversation with a user message
+
+        :param content: Content of user message (Default: None)
+        :param file: File to read content from (Default: None)
+        :return: Assistant step
+        """
+        return _AssistantOrBuildStep(self._add(Role.USER, content, file))
+
+
+def _build_with(messages: List[Message],
+                role: Literal[Role.USER, Role.ASSISTANT],
+                content: str | None = None,
+                file: str | None = None) -> Conversation:
+    """
+    Build a conversation without appending the last message to the current conversation
+
+    :param messages: Messages in the current conversation
+    :param role: Either temporary user or assistant role to the conversation
+    :param content: Content of system message (Default: None)
+    :param file: File to read content from (Default: None)
+    :return: Immutable conversation
+    """
+    content = _validate_args(content, file)
+    return Conversation(messages + [Message(role, content)])
