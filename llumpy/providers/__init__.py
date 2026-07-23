a2961ddcbf2e3d597b718c66c4543b3e3ff6836b
Derek Garcia <dcia811@gmail.com>
2026-07-23 11:38:50 -1000

    feat: Expose InvalidOllamaServerError

--- llumpy/providers/__init__.py
+++ llumpy/providers/__init__.py
@@ -1,11 +1,13 @@
 # pylint: disable=useless-import-alias, missing-module-docstring
 
 from ._anthropic import AnthropicClient as AnthropicClient, AsyncAnthropicClient as AsyncAnthropicClient
-from ._ollama import OllamaClient as OllamaClient, AsyncOllamaClient as AsyncOllamaClient
+from ._ollama import (OllamaClient as OllamaClient,
+                      AsyncOllamaClient as AsyncOllamaClient,
+                      InvalidOllamaServerError as InvalidOllamaServerError)
 from ._openai import OpenAIClient as OpenAIClient, AsyncOpenAIClient as AsyncOpenAIClient
 
 __all__ = [
     "AnthropicClient", "AsyncAnthropicClient",
-    "OllamaClient", "AsyncOllamaClient",
+    "OllamaClient", "AsyncOllamaClient", "InvalidOllamaServerError",
     "OpenAIClient", "AsyncOpenAIClient"
 ]
